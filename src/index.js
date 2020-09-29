dd002b21c4e9cc9251792060a41ba3534f549a00
IanTheG <goodwinij1@gmail.com>
2020-09-29 10:56:38 -0500

    first test for step 2

--- src/index.js
+++ src/index.js
@@ -15,6 +15,26 @@ app.get("/", (req, res) => { res.send('Hello World!')});
 
 
 
+// Creates a list of users in an object
+const mockUserData=[
+    {name:'Mark'},
+    {name:'Jill'},
+    {name:'Jose'},
+    {name:'Esperanza'}
+    ]
+
+app.get('/users', (req, res) => {
+    // (req, res) responds with a json object sending:
+    // true, a success essage, and the users
+    res.json({
+        success: true,
+        message: 'Successfully got all users. Nice!',
+        users: mockUserData
+    })
+})
+
+
+
 app.listen(3000, function() {
     console.log("server is running");
 })
