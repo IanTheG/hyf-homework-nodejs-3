eb9dcb18ffa2ea1ad569cb93320a1bcf933e9eb2
IanTheG <goodwinij1@gmail.com>
2020-09-29 11:11:30 -0500

    edited index.js for step-1

--- src/index.js
+++ src/index.js
@@ -13,27 +13,6 @@ app.use(bodyParser.json());
 // Sends Hello World! when GET requests match the root path
 app.get("/", (req, res) => { res.send('Hello World!')});
 
-
-// Creates a list of users in an object
-const mockUserData=[
-    {name:'Mark'},
-    {name:'Jill'},
-    {name:'Jose'},
-    {name:'Esperanza'}
-    ]
-
-app.get('/users', (req, res) => {
-    // (req, res) responds with a json object sending:
-    // true, a success essage, and the users
-    res.json({
-        success: true,
-        message: 'Successfully got all users. Nice!',
-        users: mockUserData
-    })
-})
-
-
-
 app.listen(3000, function() {
     console.log("server is running");
 })
