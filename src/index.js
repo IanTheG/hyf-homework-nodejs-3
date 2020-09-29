c67a821a5fcf50854d9284f097250e1c75f03424
IanTheG <goodwinij1@gmail.com>
2020-09-29 10:40:26 -0500

    updated server in index.js

--- src/index.js
+++ src/index.js
@@ -3,3 +3,18 @@ and that responds to the root url / with
 the body Hello World!. 
 Ah and would be listen to the port 3000. */
 
+const express = require('express');
+const app = express();
+
+// enables POST requests
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+// Sends Hello World! when GET requests match the root path
+app.get("/", (req, res) => { res.send('Hello World!')});
+
+
+
+app.listen(3000, function() {
+    console.log("server is running");
+})
